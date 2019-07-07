5c357093473ddc1e282f867b13227084b4fc780f
Ganesh <ganesh4212@gmail.com>
2019-07-07 06:18:11 +0000

    Initial commit

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+import Book from './book';
+import Author from './author';
+
+export { Book, Author };
---
+++ src/service/index.js
@@ -0,0 +1,4 @@
+import * as BookService from './bookService';
+import * as AuthorService from './authorService';
+
+export { BookService, AuthorService };
